204ac9757e122996d75240140ba16653963e781b
Allison Yadira <ayramireza@grupodelaware.com>
2020-05-06 18:25:22 -0500

    first commit

---
+++ src/test/java/qaclickacademy/APITest.java
@@ -0,0 +1,17 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+	
+	@Test
+	public void postJira(){
+		System.out.println("post Jira");
+	}
+	
+	@Test
+	public void deleteTwiter(){
+		System.out.println("delete Twiter");
+	}
+
+}
